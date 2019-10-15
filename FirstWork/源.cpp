f66fcaba1aafdd54ea4068df8141bbd93108e2a8
zhouyangwei123 <zhouyangwei123@users.noreply.github.com>
2019-10-15 19:40:16 +0800

    ttt

--- FirstWork/源.cpp
+++ FirstWork/源.cpp
@@ -2,16 +2,24 @@
 
 int main()
 {
-
-
 	int a, b, c;
 	printf("请输入三边长\n");
 	scanf_s("%d %d %d", &a, &b, &c);
-	printf("%d,%d,%d,", a, b, c);
-
+	//printf("%d,%d,%d,", a, b, c);
 
 	if ((a + b > c) && (b + c > a) && (a + c > b))
-		printf("可以形成三角形\n");
+	{
+		
+		if (a == b && b == c)
+			printf("等边三角形\n");
+		else if (a == b || a == c || b == c)
+			printf("等腰三角形\n");
+		else if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a))
+			printf("直角三角形\n");
+		else
+			printf("普通三角形\n");
+
+	}
 	else
 		printf("不可以");
 
